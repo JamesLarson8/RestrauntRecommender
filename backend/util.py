b006aaccf17c856486b55f25d94acdc2287c5ea1
JamesLarson8 <99367989+JamesLarson8@users.noreply.github.com>
2023-11-03 14:38:20 -0400

    Improved documentation

--- backend/util.py
+++ backend/util.py
@@ -1,5 +1,4 @@
-# utility functions / Pranay & James
-
+from cosinesim import most_similar_reviews
 # data description:
 
 #       restuarants is a list of dictionaries
@@ -21,17 +20,9 @@
     #     "categories": list,
     #     "hours": "string"
 # }
-
-
-from cosinesim import most_similar_reviews
-
-
-# this function takes in a list of restaurant names and the complete set of restaurants 
-# and returns a list of recommended restaurants
-# 
-# note that the input restaurant names are not necessarily in the list of all restaurants, so check for that
-# 
-# the input restuarants are a list of dictionaries, the definition inside is commented out above
+# This function takes in a list of restaurant names and the complete set of restaurants and returns a list of recommended restaurants.
+# Note that the input restaurant names are not necessarily in the list of all restaurants, so check for that.
+# The input restuarants are a list of dictionaries, the definition inside is commented out above.
 
 def generate_recommendations(input_restaurant, city, restaurants):
 
@@ -93,76 +84,3 @@ def SVD(tfidf):
     topic_term_matrix = svd.components_
     restaurant_topic_matrix = svd.transform(tfidf)
     return restaurant_topic_matrix
-# def generate_recommendations(restaurant_names, restaurants):
-
-#     # PLEASE WRITE YOUR CODE HERE
-
-#     # the dummy version always tells you ["McDonald's", 'Cafe Baladi', 'Chick-fil-A']
-#     print(len(restaurants))
-#     print(len(restaurant_names))
-#     for i in range (2):
-#         print(restaurant_names[i])
-#         print(restaurants[i]['name'])
-
-#     # find the restaurant business with the name as restaurant_names[0]
-#     for restaurant in restaurants:
-#         if restaurant["name"] == restaurant_names[0]:
-#             print("this is a debug",restaurant)
-#             break
-
-#     # most_similar_reviews(restaurant['business_id'], '/Users/erlich_jaso/Desktop/CityFood/4300-Template-Spring-2023/data/reviews.csv')
-    
-#     return restaurants[0:3]
-
-
-#JOANNAS GENERATE_RECOMMENDATIONS
-# def generate_recommendations(restaurant_names, restaurants):
-
-#     new_restaurants = []
-#     for restaurant_name in restaurant_names:
-#         for restaurant in restaurants:
-#             if restaurant_name.lower() in restaurant["name"].lower():
-#                 new_restaurants.append({
-#                     "name": restaurant["name"],
-#                     "latitude": float(restaurant["latitude"]),
-#                     "longitude": float(restaurant["longitude"])
-#                 })
-#                 break
-#     return new_restaurants
-
-
-
-
-
-
-# this is previously written code keeping for reference, by pranay.
-
-# def generate_recommendations(name, restaurants):
-#     # print(restaurants[0])
-#     selected_restaurant = None
-#     name_found = False
-#     recommended_restaurants = []
-#     for r in restaurants:
-#         if r['name'] == name:
-#             selected_restaurant = r
-#             name_found = True
-#             break
-#     if name_found == False:
-#         recommended_restaurants.append('Invalid Input')
-#         return recommended_restaurants
-#     else:
-#         pass
-#     for i in restaurants:
-#         if selected_restaurant['name'] == i['name']:
-#             continue
-#         else:
-#             if len(recommended_restaurants) > 4:
-#                 return recommended_restaurants
-#             set1 = set(i['categories'])
-#             set2 = set(selected_restaurant['categories'])
-#             common_elements = set1.intersection(set2)
-#         if len(common_elements) > 0:
-#             recommended_restaurants.append(i)
-#         else:
-#             pass
-#     return recommended_restaurants
